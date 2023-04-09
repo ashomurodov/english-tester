63f1499c0a1a9dc56b95a4c4717f4dedf8f27ca5
ochildevo <ochilovsardor45@gmail.com>
2023-04-09 17:31:42 +0500

    ready to logic

--- game/game.js
+++ game/game.js
@@ -1,10 +1,23 @@
 const levels = document.querySelectorAll(".level");
-const startBtn = document.querySelector(".start-btn");
 const levelBox = document.querySelector(".level-box");
 
-// Page-2
+// Page-2 variables
+const startBtn = document.querySelector(".start-btn");
 const backBtn = document.querySelector(".back");
 const page2 = document.querySelector(".page-2");
+
+// playground variables
+const playgroundSection = document.querySelector(".playground");
+const finishBtn = document.querySelector(".finish");
+const cards = document.querySelectorAll(".card");
+
+// Results page variables
+const resultSection = document.querySelector(".result-wrapper ");
+const allResultBtn = document.querySelector(".all-result-btn");
+
+// All results variable
+const allResultSection = document.querySelector(".all-results");
+const backResultArrow = document.querySelector('.back-arrow');
 
 // Page-1 listeners
 levels.forEach((level) => {
@@ -19,3 +32,46 @@ backBtn.addEventListener("click", () => {
   levelBox.classList.remove("displayNone");
   page2.classList.add("displayNone");
 });
+startBtn.addEventListener("click", () => {
+  page2.classList.add('displayNone')
+  playgroundSection.classList.remove('displayNone')
+});
+
+// Playground listeners
+finishBtn.addEventListener('click', () => {
+  resultSection.classList.remove('displayNone');
+  playgroundSection.classList.add('displayNone');
+})
+
+// function getRandomLocation() {
+//   const width = window.innerWidth;
+//   const height = window.innerHeight;
+//   const x = Math.random() * (width - 200) + 100;
+//   const y = Math.random() * (height - 200) + 100;
+//   return { x, y };
+// }
+
+function createWords() {
+  cards.forEach((item, idx) => {
+    const word = document.createElement("div");
+    word.classList.add("random-word");
+    word.textContent = "HELLO";
+    word.style.rotate = `${Math.random() * 90}deg`;
+    item.appendChild(word);
+  });
+}
+
+createWords();
+
+
+// Results Modal listeners
+allResultBtn.addEventListener('click', () => {
+  allResultSection.classList.remove('displayNone')
+  resultSection.classList.add('displayNone');
+})
+
+// All results listener
+backResultArrow.addEventListener('click', () => {
+  allResultSection.classList.add('displayNone');
+  resultSection.classList.remove('displayNone');
+})
